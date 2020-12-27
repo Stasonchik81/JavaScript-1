8c81677b34edaa720f8681c16729a76b32bb06cf
stasonchik1101 <stasonchik1101@gmail.com>
2020-12-27 14:04:34 +0300

    Решил второе задание

---
+++ GeekBrains/JS1/Lesson1/Script.js
@@ -0,0 +1,11 @@
+// Перевод температуры из С в F
+let tem_C = prompt("Введите температуру в градусах С", "температура");
+let tem_F = ( 9 / 5 ) * tem_C + 32;
+alert("Температура по Фаренгейту : " + tem_F);
+// Присваивание
+let admin = null;
+let name = null;
+name = "Василий";
+admin = name;
+console.log(admin);
+
