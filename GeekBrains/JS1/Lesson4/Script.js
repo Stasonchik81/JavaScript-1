abe06e990bb18577eb279b1235a214bde0b87f14
stasonchik1101 <stasonchik1101@gmail.com>
2021-01-07 22:50:19 +0300

    Доработал консольную бродилку

--- GeekBrains/JS1/Lesson4/Script.js
+++ GeekBrains/JS1/Lesson4/Script.js
@@ -10,9 +10,18 @@ let game = {
                 return;
             }
             let nextPoint = mover.getNextPosition(direction);
+            if(nextPoint.x < 0 || nextPoint.x >= config.colsCount){
+                nextPoint.x = player.x;
+                alert("Движение за пределы поля недопустимо!");
+            }
+            if(nextPoint.y < 0 || nextPoint.y >= config.rowsCount){
+                nextPoint.y = player.y;
+                alert("Движение за пределы поля недопустимо!");
+            }
             player.move(nextPoint);
             renderer.clear();
             renderer.render();
+            
         }
     },
 
