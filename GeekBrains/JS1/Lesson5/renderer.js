106e996f91ad09dccbf823c455bc30b22c7d7490
stasonchik1101 <stasonchik1101@gmail.com>
2021-01-09 23:04:21 +0300

    В игре Бродилка отрисовал поле и игрока

---
+++ GeekBrains/JS1/Lesson5/renderer.js
@@ -0,0 +1,27 @@
+let renderer = {
+    renderBoard(){
+        let result = this.generateBoard();
+        document.body.insertAdjacentHTML('afterbegin', result);
+        this.renderUserPosition(player);
+    },
+    generateBoard(){
+        let board = '';
+        for( let y = 0; y<config.rowsCount; y++){
+            board += '<tr>';
+        for(let x=0; x<config.colsCount; x++){
+                board += `<td data-x="${x}" data-y="${y}"></td>`;
+            }
+            board += '</tr>';
+        }
+        return `<table><tbody>${board}</tbody></table>`;
+        
+    },
+    getSquare(position){
+        return document.querySelector(`[data-x="${position.x}"][data-y="${position.y}"]`)
+    },
+
+    renderUserPosition(player){
+        let square = this.getSquare(player);
+        square.classList.add('user');
+    },
+}
