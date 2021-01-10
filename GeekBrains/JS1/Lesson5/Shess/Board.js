e40aa1ba00aa3950b4a7416ec82e25cf2201c1bc
stasonchik1101 <stasonchik1101@gmail.com>
2021-01-10 20:23:52 +0300

    Дорисовал шахматную доску

--- GeekBrains/JS1/Lesson5/Shess/Board.js
+++ GeekBrains/JS1/Lesson5/Shess/Board.js
@@ -2,16 +2,39 @@ let Board = {
     renderChessBoard(){
         let result = this.generateChessBoard();
         document.body.insertAdjacentHTML('afterbegin', result);
+        this.renderFrameSquare();
+        this.renderBlackSquare();
     },
     generateChessBoard(){
         let board = '';
-        for( let x = 0; x<8; x++){
+        for( let x = 0; x<10; x++){
             board += '<tr>';
-        for(let y=0; y<8; y++){
+        for(let y=0; y<10; y++){
                 board += `<td data-x="${x}" data-y="${y}"></td>`;
             }
             board += '</tr>';
         }
         return `<table><tbody>${board}</tbody></table>`;
+    },
+    renderFrameSquare(){
+        let chr = ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+        for (let i = 1; i<9; i++){
+            for (let j=9; j >= 0; j=j-9){
+                let square = document.querySelector(`[data-x="${i}"][data-y="${j}"]`);
+                square.innerHTML = `${i}`;
+                square = document.querySelector(`[data-x="${j}"][data-y="${i}"]`);
+                square.innerHTML = chr[i-1];
+            }    
+        } 
+    },
+    renderBlackSquare(){
+        for (let i = 1; i < 9; i=i+2){
+            for (let j = 2; j < 9; j=j+2){
+                let square = document.querySelector(`[data-x="${i}"][data-y="${j}"]`);
+                square.classList.add("black");
+                let square = document.querySelector(`[data-x="${j}"][data-y="${i}"]`);
+                square.classList.add("black");
+            }    
+        } 
     },
 }
